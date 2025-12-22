6f9f75b88c44f0e0acf9e6e41ad23dd453044034
Solomon-Cudjoe <solomoncudjoe250@gmail.com>
2025-12-22 08:33:24 +0000

    Added some assets and constants

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -19,8 +19,17 @@ export default function Index() {
           uri: "https://images.unsplash.com/photo-1764044371408-fa28e7dde471?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8",
         }}
         style={{
-          width: 100,
-          height: 100,
+          width: 200,
+          height: 200,
+          borderRadius: 100,
+        }}
+      />
+      <Image
+        source={require("@/assets/images/react-logo.png")}
+        style={{
+          width: 200,
+          height: 200,
+          borderRadius: 100,
         }}
       />
     </View>
